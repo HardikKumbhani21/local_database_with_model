15d156b9ec9b02867867ddf35eaf15e02ddbe858
HardikKumbhani21 <117890928+HardikKumbhani21@users.noreply.github.com>
2022-12-28 21:46:53 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:database_model/view/add_todo.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = await openDatabase(
+    join(await getDatabasesPath(), 'myapp.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE todo(id INTEGER PRIMARY KEY AUTOINCREMENT, title varchar(255), description varchar(255))',
+      );
+    },
+    version: 1,
+  );
+
+  Get.put(database);
+
+  runApp(MyApp(
+    database: database,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required this.database});
+
+  final Database database;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: AddTodo(
+          database: database,
+        ));
+  }
+}
